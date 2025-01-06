c2d1bce953f08e81c57afaba86c0aed84b17052b
Luis Eduardo Braga <155861742+luisbragac@users.noreply.github.com>
2025-01-06 09:19:23 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+import requests
+from deep_translator import GoogleTranslator
+
+app = FastAPI()
+
+
+@app.get("/kanye-west")
+def frases_kanye():
+    url = ('https://api.kanye.rest/')
+
+
+    
+    try:
+        # Faz a requisição à API do kanye
+        response = requests.get(url)
+        
+        # Exceção para códigos de erro HTTP
+        response.raise_for_status()
+
+        dados = response.json()
+
+        # Extrai a citação do JSON retornado
+        aspas = dados['quote']
+
+        # Traduz a citação para português
+        traduzido = GoogleTranslator(source='en', target='pt').translate(aspas)
+
+        return{
+                'Original':aspas,
+                'Tradução': traduzido   
+        }
+
+    except requests.exceptions.RequestException as e:
+        return {"error": f"Erro na requisição: {str(e)}"}
+
+    except Exception as e:
+        print(f'Algo deu errado com a requisição. Código: {response.status_code} - Erro: {e}')
